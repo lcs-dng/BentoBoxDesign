2ace09d73e0b13231c59ec282bdb40f050105103
Dexter Ng <DNg@lcs.on.ca>
2023-10-29 16:01:59 -0400

    finished the basic layout of the bento box and added colour to the rectangles.

--- BentoBoxDesign/ContentView.swift
+++ BentoBoxDesign/ContentView.swift
@@ -8,13 +8,95 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let lightGray = Color(
+         hue: 0/360.0,
+         saturation: 0.0,
+         brightness: 0.93
+     )
+     let mediumGray = Color(
+         hue: 0/360.0,
+         saturation: 0.0,
+         brightness: 0.8
+     )
+     let darkGray = Color(
+         hue: 0/360.0,
+         saturation: 0.0,
+         brightness: 0.45
+     )
+     let lightPurple = Color(
+         hue: 266.0/360.0,
+         saturation: 0.0,
+         brightness: 1.0
+     )
+     let mediumPurple = Color(
+         hue: 266.0/360.0,
+         saturation: 0.35,
+         brightness: 0.50
+     )
+     let darkPurple = Color(
+         hue: 286.0/360.0,
+         saturation: 0.65,
+         brightness: 0.50
+     )
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack {
+                    HStack {
+                        Rectangle()
+                            .fill(mediumGray)
+                        Rectangle()
+                            .fill(mediumGray)
+                    }
+                    .frame(height: 90)
+                    Rectangle()
+                        .fill(mediumGray)
+                    HStack {
+                        Rectangle()
+                            .fill(mediumGray)
+                        Rectangle()
+                            .fill(mediumGray)
+                    }
+                    .frame(height: 90)
+                    Rectangle()
+                        .fill(mediumGray)
+                        .frame(height: 65)
+                }
+                VStack {
+                    HStack {
+                        Rectangle()
+                            .fill(mediumGray)
+                            .frame(width: 115)
+                        Rectangle()
+                            .fill(mediumGray)
+                    }
+                    .frame(height: 90)
+                    Rectangle()
+                        .fill(mediumGray)
+                    Rectangle()
+                        .fill(mediumGray)
+                        .frame(height: 65)
+                }
+                .frame(width: 300)
+                VStack {
+                    Rectangle()
+                        .fill(mediumGray)
+                        .frame(height: 90)
+                    HStack {
+                        Rectangle()
+                            .fill(mediumGray)
+                        Rectangle()
+                            .fill(mediumGray)
+                    }
+                    Rectangle()
+                        .fill(mediumGray)
+                        .frame(height: 125)
+                }
+            }
         }
+        .frame(width: 800, height: 400)
         .padding()
     }
 }
